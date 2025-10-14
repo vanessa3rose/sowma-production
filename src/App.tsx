4ee2a3cfc861595850a19439146a74a45cce0d8e
Joshua Paul Guirao <joshuaguirao24@gmail.com>
2025-10-14 15:12:28 -0400

    Social Media Page Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Homepage from "./pages/Homepage";
 import { Route, Switch } from "wouter";
+import SocialMediaPage from "./pages/SocialMediaPage";
 
 const App = () => {
   return (
     <>
       <Switch>
         <Route path="/" component={Homepage} />
+        <Route path="/social-media" component={SocialMediaPage} />
 
         {/* Shows a 404 error if the path doesn't match anything */}
         {
