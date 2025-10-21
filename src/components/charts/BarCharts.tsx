b7fb5785ad13fac3732bbdb5ecea934ea0e34460
kalenlauring <lauringkalen@gmail.com>
2025-10-21 00:28:43 -0400

    implemented charts

---
+++ src/components/charts/BarCharts.tsx
@@ -0,0 +1,45 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+type BarGraphsProps = {
+  data: any[]; // define as array of integers
+  width: number;
+  height: number;
+  dataKeys: string[]; // name of each bar label
+  xAxisKey: string; // the x axis titles for the chart
+};
+
+const BarCharts = ({
+  data,
+  width,
+  height,
+  dataKeys,
+  xAxisKey,
+}: BarGraphsProps) => {
+  return (
+    <ResponsiveContainer width={width} height={height}>
+      <BarChart
+        data={data}
+        margin={{ top: 20, right: 50, left: 20, bottom: 20 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey={xAxisKey}
+          tick={{ fontFamily: "Poppins, sans-serif" }}
+        />
+        <YAxis />
+        {dataKeys.map((key) => (
+          <Bar key={key} dataKey={key} fill="#7987FF" />
+        ))}
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarCharts;
