59ae1ed12896fb3a9f5cf88651ddbd67f5bd2baa
Cole Hamilton <cole.hamilton@tufts.edu>
2025-10-26 14:30:13 -0400

    Added sidebar/header boolean checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { Route, Switch } from "wouter";
+import { Route, Switch, useLocation } from "wouter";
 
-// import LeftSidebar from "./components/LeftSidebar";
-// import TopBanner from "./components/TopBanner";
+import LeftSidebar from "./components/LeftSidebar";
+import TopBanner from "./components/TopBanner";
 
 import Homepage from "./pages/Homepage";
 import SocialMediaPage from "./pages/SocialMediaPage";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage"
 
 const App = () => {
+  const [location] = useLocation();
+  const hideLayoutRoutes = ["/login"];
+  const hideLayout = hideLayoutRoutes.includes(location);
+  
   return (
     <>
-      {/* <LeftSidebar /> */}
-      {/* <TopBanner /> */}
+      {!hideLayout && <LeftSidebar />}
+      {!hideLayout && <TopBanner />}
 
       <Switch>
         <Route path="/" component={Homepage} />
