04a28e209d4a9e30c653df844eadfa81e25ddcc5
vienna <viennalau0@gmail.com>
2026-01-30 15:23:06 -0500

    collapsible sidebar & focus indicators

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,6 +1,8 @@
 import { useClerk } from "@clerk/clerk-react";
+import { useState } from "react";
+import { useLocation, Link } from "wouter";
 
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-cropped.png";
 import facebook from "../assets/facebook.jpg";
 import google from "../assets/google.jpg";
 import instagram from "../assets/instagram.jpg";
@@ -26,14 +28,24 @@ const exceptionRoutes: Record<string, string> = {
   newsletter: "/newsletter",
 };
 
-const LeftSidebar = ({ mobile = false, open = false, onClose = () => {} }) => {
-  const sidebarClasses = mobile
-    ? `fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50
-       transform transition-transform duration-300 p-4 border-r border-gray-600 pl-12
-       ${open ? "translate-x-0" : "-translate-x-full"}`
-    : "fixed top-0 left-0 flex flex-col w-1/5 space-y-5 h-full items-center bg-white border-r border-gray-600 p-4 z-50";
 
+const LeftSidebar = ({ mobile = false, open = false, onClose = () => {} }) => {
+  const [collapsed, setCollapsed] = useState(false);
   const { signOut } = useClerk();
+  const [location] = useLocation();
+
+  const isDashboardActive = location === "/";
+  const isGlossaryActive = location === "/glossary";
+  
+
+  const sidebarClasses = mobile
+  ? `fixed top-0 left-0 h-full w-64 bg-white shadow-2xl z-50
+     transform transition-transform duration-300 p-4 border-r border-gray-100 pl-12
+     ${open ? "translate-x-0" : "-translate-x-full"}`
+  : `fixed top-6 left-4 h-full bg-white z-50 transition-all duration-300 
+      shadow-[0px_6px_16px_0px_rgba(0,0,0,0.25)]
+      rounded-xl border-gray-100
+     ${collapsed ? "w-20" : "w-1/5"} p-4 flex flex-col items-center`;
 
   const handleSignOut = async () => {
     await signOut();
@@ -48,24 +60,67 @@ const LeftSidebar = ({ mobile = false, open = false, onClose = () => {} }) => {
       )}
 
       <div className={sidebarClasses}>
-        {/* Logo */}
-        <img
-          src={logo}
-          alt="Logo"
-          className="sm:w-full lg:w-4/5 aspect-square object-contain m-2"
-        />
+        {/* Collapse Arrow */}
+        {!mobile && (
+            <div
+              className={`flex w-full
+                ${collapsed ? "justify-center py-4" : "justify-end"}
+              `}
+            >
+              <button
+                onClick={() => setCollapsed(!collapsed)}
+                className="w-6 h-6 border-2 border-[#A1A1A1]
+                          flex items-center justify-center
+                          text-[#A1A1A1] hover:bg-gray-100
+                          transition rounded-sm"
+              >
+                <span
+                  className={`transform transition-transform ${
+                    collapsed ? "rotate-180" : ""
+                  }`}
+                  >
+                &lt;
+              </span>
+            </button>
+          </div>
+        )}
 
         {/* Navigation */}
-        <nav className="flex flex-col w-4/5 items-start space-y-3 px-1">
-          <a href="/">
-            <div className="bg-light-gray flex flex-row items-center gap-x-4">
+        <nav
+          className={`relative flex flex-col px-1 transition-all 
+          flex-1 min-h-0
+            ${collapsed ? "w-full items-center gap-3" : "w-4/5 items-start space-y-3"}
+          `}
+        >
+        
+        {/* logo */}
+        {!collapsed && (
+          <img
+          src={logo}
+          alt="Logo"
+          className="object-contain mx-auto transition-all p-2"/>
+        )}
+        
+
+        <Link href="/" className="w-full flex justify-center">
+          <div
+            className={`flex items-center rounded-xl transition-all
+              ${collapsed
+                ? "w-12 h-12 justify-center"
+                : "w-full gap-x-4 p-3"}
+              ${isDashboardActive
+                ? "bg-[#4781C2] text-white shadow-lg"
+                : "hover:bg-gray-100"}
+            `}
+          >
+
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6"
+                className="size-6 shrink-0"
               >
                 <path
                   strokeLinecap="round"
@@ -73,21 +128,28 @@ const LeftSidebar = ({ mobile = false, open = false, onClose = () => {} }) => {
                   d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                 />
               </svg>
-              <p className="md:hidden lg:block font-poppins text-[20px] font-medium">
-                Dashboard
-              </p>
+              {!collapsed && (
+                <p className="md:hidden lg:block font-poppins text-[20px] font-medium">
+                  Dashboard
+                </p>
+              )}
             </div>
-          </a>
+          </Link>
 
-          <a href="/glossary">
-            <div className="bg-light-gray flex flex-row items-center gap-x-4">
+          <Link href="/glossary" className="w-full">
+            <div className={`flex flex-row items-center gap-x-4 p-3 rounded-xl transition-all
+            ${collapsed ? "justify-center" : "gap-x-4"}
+              ${isGlossaryActive 
+                ? "bg-[#4781C2] text-white shadow-lg" 
+                : "bg-transparent hover:bg-gray-100"}
+            `}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6"
+                className="size-6 shrink-0"
               >
                 <path
                   strokeLinecap="round"
@@ -95,51 +157,86 @@ const LeftSidebar = ({ mobile = false, open = false, onClose = () => {} }) => {
                   d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
                 />
               </svg>
-              <p className="md:hidden lg:block font-poppins text-[20px] font-medium">
-                Glossary
-              </p>
+              {!collapsed && (
+                <p className="md:hidden lg:block font-poppins text-[20px] font-medium">
+                  Glossary
+                </p>
+              )}
             </div>
-          </a>
+          </Link>
 
           {/* Socials */}
-          <div className="flex flex-col space-y-3 w-full">
-            <h1 className="font-poppins font-medium text-[20px] text-[#626262]">
-              PLATFORMS
-            </h1>
+          <div className="flex flex-col space-y-3">
+            {collapsed ? (
+              <div className="md:hidden w-8 border-t border-gray-300 my-3 mx-auto" />
+            ) : (
+              <h1 className="font-poppins font-medium text-[20px] text-[#626262]">
+                PLATFORMS
+              </h1>
+            )}
+
             {socialLinks.map((social, idx) => {
               const href =
                 exceptionRoutes[social.slug] || `/social/${social.slug}`; // fallback to default
+                const isActive = location === href;
 
               return (
-                <a href={href} key={idx} className="flex items-center gap-4">
+                <Link
+                  href={href}
+                  key={idx}
+                  className={`relative flex items-center p-2 rounded-xl transition-all duration-200
+                    ${collapsed ? "justify-center w-12 mx-auto" : "gap-4 w-full"}
+                    ${isActive
+                      ? "bg-[#F0F0F0] text-black shadow-lg"
+                      : "bg-transparent hover:bg-gray-100"
+                    }
+                  `}
+                >
                   <img
                     src={social.icon}
                     alt={social.label}
-                    className="w-10 h-10 hover:opacity-80 transition rounded-[10px] border border-solid"
+                    className="w-9 h-9 object-contain rounded-[10px] bg-white p-1 shrink-0"
                   />
-                  <p className="md:hidden lg:block font-poppins font-medium text-[20px]">
-                    {social.label}
-                  </p>
-                </a>
+                  {/* The Blue Indicator Bar */}
+                  {isActive && (
+                    <div className="absolute -left-4 top-1/2 -translate-y-1/2 w-2 h-4/5 bg-[#4781C2] rounded-full" />
+                  )}
+                  {!collapsed && (
+                    <p className="md:hidden lg:block font-poppins font-medium text-[20px]">
+                      {social.label}
+                    </p>
+                  )}
+                </Link>
               );
             })}
           </div>
 
           {/* Sign Out */}
-          <div
-            className={`absolute bottom-0 pb-8 justify-center flex ${mobile ? "pl-6" : "w-full"}`}
-          >
+          <div className="mt-auto pb-8 flex justify-center w-full">
+
             <button
               onClick={handleSignOut}
-              className="flex items-center gap-3 text-[#626262] font-medium hover:text-red-800 cursor-pointer"
+              className={`flex items-center rounded-xl transition-all
+                ${collapsed ? "w-12 h-12 justify-center" : "gap-3 px-3 py-2"}
+                text-[#626262] hover:text-red-800 hover:bg-gray-100`}
             >
+
+          <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 shrink-0"
+              >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6A2.25 2.25 0 0 0 5.25 5.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H3"
               />
+            </svg>
 
-              <span className="md:hidden lg:block text-[18px]">Sign Out</span>
+              {!collapsed && <span className="md:hidden lg:block text-[18px]">Sign Out</span>}
             </button>
           </div>
         </nav>
