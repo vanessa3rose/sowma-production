6b4379e99f64ecb69bca964bca899d5f29d62948
Lakshita Jain <lak.jai110@gmail.com>
2026-03-08 10:41:41 -0400

    add LinkedIn provider to social media configuration

--- scripts/social-media-setup.ts
+++ scripts/social-media-setup.ts
@@ -50,6 +50,14 @@ const SOCIAL_MEDIA_CONFIG = [
     profileUrl: "https://www.constantcontact.com/",
     email: null,
   },
+  {
+    provider: Provider.LINKEDIN,
+    userId: "1640840",
+    username: "schoolonwheelsofmasschusetts",
+    displayName: "School on Wheels of Massachusetts",
+    profileUrl: "https://www.linkedin.com/company/schoolonwheelsofmasschusettss/",
+    email: null,
+  }
 ];
 
 /**
